f473f8d7cbf52656775f159c7c01e40c6ed4297b
ckx-sec <13911336781@163.com>
2022-08-02 08:54:10 +0800

    test

--- lib.rs
+++ lib.rs
@@ -1,74 +1,196 @@
 #![cfg_attr(not(feature = "std"), no_std)]
 
+use ink_env::Environment;
 use ink_lang as ink;
+use serde_json::Value;
 
-#[ink::contract]
-mod ink_couch {
+#[ink::chain_extension]
+pub trait CouchBridge {
+    type ErrorCode = CouchErr;
 
-    /// Defines the storage of your contract.
-    /// Add new fields to the below struct in order
-    /// to add new static storage fields to your contract.
-    #[ink(storage)]
-    pub struct InkCouch {
-        /// Stores a single `bool` value on the storage.
-        value: bool,
+    #[ink(extension = 2, returns_result = false)]
+    fn db_find_raw(subject: String) -> String;
+
+    #[ink(extension = 3, returns_result = false)]
+    fn db_create_raw(subject: String) -> String;
+}
+
+#[derive(Debug, Copy, Clone, PartialEq, Eq, scale::Encode, scale::Decode)]
+#[cfg_attr(feature = "std", derive(scale_info::TypeInfo))]
+pub enum CouchErr {
+    FailGetRandomSource,
+}
+
+impl ink_env::chain_extension::FromStatusCode for CouchErr {
+    fn from_status_code(status_code: u32) -> Result<(), Self> {
+        match status_code {
+            0 => Ok(()),
+            1 => Err(Self::FailGetRandomSource),
+            _ => panic!("encountered unknown status code"),
+        }
     }
+}
 
-    impl InkCouch {
-        /// Constructor that initializes the `bool` value to the given `init_value`.
+#[derive(Debug, Clone, PartialEq, Eq)]
+#[cfg_attr(feature = "std", derive(scale_info::TypeInfo))]
+pub enum CustomEnvironment {}
+
+impl Environment for CustomEnvironment {
+    const MAX_EVENT_TOPICS: usize = <ink_env::DefaultEnvironment as Environment>::MAX_EVENT_TOPICS;
+
+    type AccountId = <ink_env::DefaultEnvironment as Environment>::AccountId;
+    type Balance = <ink_env::DefaultEnvironment as Environment>::Balance;
+    type Hash = <ink_env::DefaultEnvironment as Environment>::Hash;
+    type BlockNumber = <ink_env::DefaultEnvironment as Environment>::BlockNumber;
+    type Timestamp = <ink_env::DefaultEnvironment as Environment>::Timestamp;
+
+    type ChainExtension = CouchBridge;
+}
+
+#[ink::contract(env = crate::CustomEnvironment)]
+mod my_contract {
+    use super::CouchErr;
+    use couch_rs::types::find::FindQuery;
+    use serde_json::{json, Value};
+
+    #[ink(storage)]
+    pub struct MyContract {}
+
+    impl MyContract {
         #[ink(constructor)]
-        pub fn new(init_value: bool) -> Self {
-            Self { value: init_value }
+        pub fn new() -> Self {
+            Self {}
         }
 
-        /// Constructor that initializes the `bool` value to `false`.
-        ///
-        /// Constructors can delegate to other constructors.
-        #[ink(constructor)]
-        pub fn default() -> Self {
-            Self::new(Default::default())
-        }
-
-        /// A message that can be called on instantiated contracts.
-        /// This one flips the value of the stored `bool` from `true`
-        /// to `false` and vice versa.
         #[ink(message)]
-        pub fn flip(&mut self) {
-            self.value = !self.value;
+        pub fn find_all(&self) -> String {
+            let find_all = FindQuery::find_all();
+            let find_all_str = find_all.as_value().to_string();
+            let result_raw = self.env().extension().db_find_raw(find_all_str).unwrap();
+            //let result: Value = serde_json::from_slice(&result_raw).unwrap();
+            let result_value = serde_json::from_str::<Value>(&result_raw).unwrap();
+            serde_json::to_string_pretty(&result_value).unwrap()
         }
 
-        /// Simply returns the current value of our `bool`.
         #[ink(message)]
-        pub fn get(&self) -> bool {
-            self.value
+        pub fn create(&self) -> String {
+            let test_doc = json!({
+                "ink":"substrate",
+                "this":"that",
+                "asdf":"zxcv"
+            });
+            self.env()
+                .extension()
+                .db_create_raw(test_doc.to_string())
+                .unwrap()
         }
     }
 
     /// Unit tests in Rust are normally defined within such a `#[cfg(test)]`
-    /// module and test functions are marked with a `#[test]` attribute.
-    /// The below code is technically just normal Rust code.
     #[cfg(test)]
     mod tests {
+        use crate::my_contract::*;
         /// Imports all the definitions from the outer scope so we can use them here.
-        use super::*;
-
-        /// Imports `ink_lang` so we can use `#[ink::test]`.
+        use couch_rs::types::find::FindQuery;
         use ink_lang as ink;
+        use serde_json::{json, Value};
+        // use futures::executor::block_on;
 
-        /// We test if the default constructor does its job.
         #[ink::test]
-        fn default_works() {
-            let ink_couch = InkCouch::default();
-            assert_eq!(ink_couch.get(), false);
+        fn find_all_works() {
+            struct FindExtension;
+            impl ink_env::test::ChainExtension for FindExtension {
+                fn func_id(&self) -> u32 {
+                    2
+                }
+
+                fn call(&mut self, _input: &[u8], output: &mut Vec<u8>) -> u32 {
+                    const TEST_DB: &str = "test_db";
+                    let runtime = tokio::runtime::Builder::new_current_thread()
+                        .enable_all()
+                        .build()
+                        .unwrap();
+                    runtime.block_on(async {
+                        use scale::Decode;
+
+                        let client = couch_rs::Client::new_local_test().unwrap();
+                        let db = client.db(TEST_DB).await.unwrap();
+
+                        let query_value = {
+                            let mut input = _input;
+                            // 这里ink把input编码了两次，原因不明，只好解码两次
+                            let input_decode: Vec<u8> = Vec::decode(&mut input).unwrap();
+                            let query_str = String::decode(&mut input_decode.as_slice()).unwrap();
+                            let mut value: Value = serde_json::from_str(&query_str).unwrap();
+
+                            let obj = value.as_object_mut().unwrap();
+                            if !obj.contains_key("sort") {
+                                obj.insert("sort".to_owned(), json!([]));
+                            };
+
+                            value
+                        };
+
+                        let query = FindQuery::new_from_value(query_value);
+                        let docs = db.find::<Value>(&query).await.unwrap();
+
+                        let result = {
+                            let result_raw = docs.get_data();
+                            serde_json::to_string(result_raw).unwrap()
+                        };
+                        scale::Encode::encode_to(&result, output);
+                    });
+
+                    0
+                }
+            }
+            ink_env::test::register_chain_extension(FindExtension);
+            let contract = MyContract::new();
+            println!("{}", contract.find_all());
         }
 
-        /// We test a simple use case of our contract.
         #[ink::test]
-        fn it_works() {
-            let mut ink_couch = InkCouch::new(false);
-            assert_eq!(ink_couch.get(), false);
-            ink_couch.flip();
-            assert_eq!(ink_couch.get(), true);
+        fn create_works() {
+            struct CreateExtension;
+            impl ink_env::test::ChainExtension for CreateExtension {
+                fn func_id(&self) -> u32 {
+                    3
+                }
+
+                fn call(&mut self, _input: &[u8], output: &mut Vec<u8>) -> u32 {
+                    const TEST_DB: &str = "test_db";
+                    let runtime = tokio::runtime::Builder::new_current_thread()
+                        .enable_all()
+                        .build()
+                        .unwrap();
+
+                    runtime.block_on(async {
+                        let client = couch_rs::Client::new_local_test().unwrap();
+                        let db = client.db(TEST_DB).await.unwrap();
+
+                        use scale::Decode;
+
+                        let mut doc: Value = {
+                            let mut input = _input;
+                            // 这里ink把input编码了两次，原因不明，只好解码两次
+                            let input_decode: Vec<u8> = Vec::decode(&mut input).unwrap();
+                            let query_raw = String::decode(&mut input_decode.as_slice()).unwrap();
+                            serde_json::from_str(&query_raw).unwrap()
+                        };
+
+                        let result = {
+                            let result_raw = db.create(&mut doc).await.unwrap();
+                            serde_json::to_string(&result_raw).unwrap()
+                        };
+                        scale::Encode::encode_to(&result, output);
+                    });
+
+                    0
+                }
+            }
+            ink_env::test::register_chain_extension(CreateExtension);
+            let contract = MyContract::new();
+            println!("{}", contract.create());
         }
     }
 }
